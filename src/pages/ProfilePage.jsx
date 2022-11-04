2e599b41e620c7a5c816042fcca79c8fee359c2e
Praise25 <anenepraise1@gmail.com>
2022-11-04 21:59:23 +0100

    feat: implement profile page

---
+++ src/pages/ProfilePage.jsx
@@ -0,0 +1,12 @@
+import Boilerplate from "../components/Layout/Boilerplate";
+import Profile from "../components/Profile";
+
+const ProfilePage = () => {
+  return (
+    <Boilerplate>
+      <Profile />
+    </Boilerplate>
+  )
+}
+
+export default ProfilePage;
