7de381e0598da6c42977c104242b30d0e8d4b1e2
Praise25 <anenepraise1@gmail.com>
2022-11-04 21:47:30 +0100

    feat: implement boilerplate

---
+++ src/components/Layout/Boilerplate.jsx
@@ -0,0 +1,14 @@
+import { Fragment } from "react";
+import classes from "./Boilerplate.module.css";
+import Footer from "../Footer";
+
+const Boilerplate = (props) => {
+  <Fragment>
+    <div className={classes["app-container"]}>
+      {props.children}
+    </div>
+    <Footer />
+  </Fragment>;
+};
+
+export default Boilerplate;
