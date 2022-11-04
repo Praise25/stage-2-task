31a683efb6ee5e8cb26ca1cd3d8d8f95c1296581
Praise25 <anenepraise1@gmail.com>
2022-11-04 22:08:50 +0100

    fix: resolve blank rendering issue

--- src/components/Layout/Boilerplate.jsx
+++ src/components/Layout/Boilerplate.jsx
@@ -3,12 +3,12 @@ import classes from "./Boilerplate.module.css";
 import Footer from "./Footer";
 
 const Boilerplate = (props) => {
-  <Fragment>
-    <div className={classes["app-container"]}>
-      {props.children}
-    </div>
-    <Footer />
-  </Fragment>;
+  return (
+    <Fragment>
+      <div className={classes["app-container"]}>{props.children}</div>
+      <Footer />
+    </Fragment>
+  );
 };
 
 export default Boilerplate;
